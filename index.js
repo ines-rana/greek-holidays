a3662508083493b6256273d4d02468142975e853
ines-rana <57879729+ines-rana@users.noreply.github.com>
2019-11-18 23:04:53 +0000

    .

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
   //.use(express.static(path.join(__dirname, 'public')))
   //.set('views', path.join(__dirname, 'views'))
   //.set('view engine', 'ejs')
@@ -14,3 +13,4 @@ express()
 	res.append('Access-Control-Allow-Headers', 'Content-Type');
   	res.send('Hello World 3!\n');
   })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
