d57f440f6d7d019c54a910608ced41e239a0846a
ines-rana <57879729+ines-rana@users.noreply.github.com>
2019-11-18 22:47:02 +0000

    .

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
+  //.use(express.static(path.join(__dirname, 'public')))
+  //.set('views', path.join(__dirname, 'views'))
+  //.set('view engine', 'ejs')
   .get('/', function (req, res) {
-  	res.send('Hello World!');
+  	res.send('Hello World 2!');
   })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  //.listen(PORT, () => console.log(`Listening on ${ PORT }`))
