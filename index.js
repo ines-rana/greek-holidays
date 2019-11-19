1733f2a822ea46619d0273c3122079bd23e4542b
ines-rana <57879729+ines-rana@users.noreply.github.com>
2019-11-19 00:54:19 +0000

    .

--- index.js
+++ index.js
@@ -8,17 +8,21 @@ express()
   //.set('views', path.join(__dirname, 'views'))
   //.set('view engine', 'ejs')
   .get('/', (req, res, next) => {
-	res.append('Content-Type', 'text/plain');
+	res.append('Content-Type', 'text/plain; charset=utf-8');
 		// allow XHR calls (CORS)
 	res.append('Access-Control-Allow-Origin', ['*']);
 	res.append('Access-Control-Allow-Methods', 'GET');
 	res.append('Access-Control-Allow-Headers', 'Content-Type');
 
 	var now = moment().tz('Europe/Athens');
-  	res.send('Hello World 5!\n' +
-		now.format('') + '\n' +
-		now.format('MMMM MM/DD/YYYY hh:mm:ss a') + '\n' +
-		now.format('YYYY') + '\n'
+	const thisYear=now.format('YYYY');
+
+  	res.send(''
+		+ now.format('') + '\n' 
+		+ now.format('MMMM MM/DD/YYYY hh:mm:ss a') + '\n'
+		+ thisYear + '\n'
+		+ req.query.from + '\n'
+		+ req.query.to + '\n'
         );
   })
   .listen(PORT  /*, () => console.log(`Listening on ${ PORT }`)*/)
