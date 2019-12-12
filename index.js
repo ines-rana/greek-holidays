6b63c60e75cae6bea7699544c0a188da1cd5b58f
ines-rana <57879729+ines-rana@users.noreply.github.com>
2019-12-12 06:29:14 +0000

    .

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // produce a list of official greek holidays in iCalendar (RFC 5545) format
-
+// Content-Type: text/calendar; charset="UTF-8"
 
 const express = require('express')
 const path = require('path')
@@ -17,61 +17,6 @@ const grTZ = 'Europe/Athens';			// timezone in Greece
 
 const uuidv1 = require('uuid/v1');		// uuidv1();
 
-/* https://tools.ietf.org/html/rfc5545
-
-LOCATION;LANGUAGE=el:�����
-       DTSTART;TZID=America/New_York:19980119T020000
-       DTEND;TZID=America/New_York:19980119T030000
-
-DTSTART;TZID=America/New_York:19970105T083000
-RRULE:FREQ=YEARLY;INTERVAL=2;BYMONTH=1;BYDAY=SU;BYHOUR=8,9;
- BYMINUTE=30
-"every Sunday in January at 8:30 AM and 9:30 AM, every other year"
-
-RRULE:FREQ=YEARLY
-
-
-       BEGIN:VCALENDAR
-       VERSION:2.0
-       PRODID:-//hacksw/handcal//NONSGML v1.0//EN
-       BEGIN:VEVENT
-       UID:19970610T172345Z-AF23B2@example.com
-       DTSTAMP:19970610T172345Z
-       DTSTART:19970714T170000Z
-       DTEND:19970715T040000Z
-       SUMMARY:Bastille Day Party
-       END:VEVENT
-       END:VCALENDAR
-
-
-Sample calendar:
-
-
-BEGIN:VEVENT
-CLASS:PUBLIC
-UID:2019-01-01GR415regcountry@www.officeholidays.com
-CREATED:20191118T200338Z
-DESCRIPTION: New Year's Day is a public holiday in all countries that observe the Gregorian calendar, with the exception of Israel\n\n\n\nInformation provided by www.officeholidays.com
-URL:https://www.officeholidays.com/holidays/greece/international-new-years-day
-DTSTART;VALUE=DATE:20190101
-DTEND;VALUE=DATE:20190102
-DTSTAMP:20080101T000000Z
-LOCATION;LANGUAGE=el:Ελλάς
-PRIORITY:5
-SEQUENCE:0
-SUMMARY;LANGUAGE=el:Πρωτοχρονιά
-TRANSP:OPAQUE
-X-MICROSOFT-CDO-BUSYSTATUS:BUSY
-X-MICROSOFT-CDO-IMPORTANCE:1
-X-MICROSOFT-DISALLOW-COUNTER:FALSE
-X-MS-OLK-ALLOWEXTERNCHECK:TRUE
-X-MS-OLK-AUTOFILLLOCATION:FALSE
-X-MICROSOFT-CDO-ALLDAYEVENT:TRUE
-X-MICROSOFT-MSNCALENDAR-ALLDAYEVENT:TRUE
-X-MS-OLK-CONFTYPE:0
-END:VEVENT
-*/
-
 
 
 // υπολογισμός ημέρας/μήνα του ελληνορθοδόξου Πάσχα για το έτος y
@@ -81,7 +26,7 @@ function greek_easter(y){
 if (y<1923 || y>4099){return("year must be between 1923 and 4099");}
 
 var retval = {};
-var e=0, y2=0, G=0, I=0, J=0, L=0, p=0, d=0, m=0;   // make sure they are used as numbers (rather than strings)
+var e=0, y2=0, G=0, I=0, J=0, L=0, p=0, d=0, m=0;
 
 e=10
 if (y>1600) {
@@ -101,6 +46,9 @@ m=3+Math.floor((p+26)/30)
 retval["year"]=y; retval["month"]=m; retval["date"]=d;
 return retval;
 }
+
+
+
 
 
 // format date string in local timezone (1997-01-06) as 19970105T220000Z
@@ -186,16 +134,6 @@ return list
 
 
 
-const vcal_event = (function () {/*BEGIN:VEVENT
-DESCRIPTION;LANGUAGE=el:summary
-SUMMARY;LANGUAGE=el:summary
-DTSTART:dtstart
-DTEND:dtend
-COMMENT;LANGUAGE=el:comment
-UID:uid
-DTSTAMP:dtstamp
-END:VEVENT*/}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1];
-
 
 const vcal_header = (function () {/*BEGIN:VCALENDAR
 VERSION:2.0
@@ -210,7 +148,20 @@ METHOD:PUBLISH
 LOCATION;LANGUAGE=el:Ελλάς
 */}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1];
 
+const vcal_event = (function () {/*BEGIN:VEVENT
+DESCRIPTION;LANGUAGE=el:summary
+SUMMARY;LANGUAGE=el:summary
+DTSTART:dtstart
+DTEND:dtend
+COMMENT;LANGUAGE=el:comment
+UID:uid
+DTSTAMP:dtstamp
+END:VEVENT*/}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1];
+
 const vcal_footer = '\nEND:VCALENDAR\r';
+
+
+
 
 express()
   .disable('x-powered-by')
@@ -298,4 +249,66 @@ express()
 		+ JSON.stringify(hList) + '\n'
 		+ fromYear + " - " + toYear + '\n'
         );
+*/
+
+
+
+/* https://tools.ietf.org/html/rfc5545
+
+Samples:
+
+LOCATION;LANGUAGE=el:�����
+       DTSTART;TZID=America/New_York:19980119T020000
+       DTEND;TZID=America/New_York:19980119T030000
+
+DTSTART;TZID=America/New_York:19970105T083000
+RRULE:FREQ=YEARLY;INTERVAL=2;BYMONTH=1;BYDAY=SU;BYHOUR=8,9;
+ BYMINUTE=30
+"every Sunday in January at 8:30 AM and 9:30 AM, every other year"
+
+RRULE:FREQ=YEARLY
+
+
+
+Sample event:
+
+BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//hacksw/handcal//NONSGML v1.0//EN
+BEGIN:VEVENT
+UID:19970610T172345Z-AF23B2@example.com
+DTSTAMP:19970610T172345Z
+DTSTART:19970714T170000Z
+DTEND:19970715T040000Z
+SUMMARY:Bastille Day Party
+END:VEVENT
+END:VCALENDAR
+
+
+Sample calendar:
+
+
+BEGIN:VEVENT
+CLASS:PUBLIC
+UID:2019-01-01GR415regcountry@www.officeholidays.com
+CREATED:20191118T200338Z
+DESCRIPTION: New Year's Day is a public holiday in all countries that observe the Gregorian calendar, with the exception of Israel\n\n\n\nInformation provided by www.officeholidays.com
+URL:https://www.officeholidays.com/holidays/greece/international-new-years-day
+DTSTART;VALUE=DATE:20190101
+DTEND;VALUE=DATE:20190102
+DTSTAMP:20080101T000000Z
+LOCATION;LANGUAGE=el:Ελλάς
+PRIORITY:5
+SEQUENCE:0
+SUMMARY;LANGUAGE=el:Πρωτοχρονιά
+TRANSP:OPAQUE
+X-MICROSOFT-CDO-BUSYSTATUS:BUSY
+X-MICROSOFT-CDO-IMPORTANCE:1
+X-MICROSOFT-DISALLOW-COUNTER:FALSE
+X-MS-OLK-ALLOWEXTERNCHECK:TRUE
+X-MS-OLK-AUTOFILLLOCATION:FALSE
+X-MICROSOFT-CDO-ALLDAYEVENT:TRUE
+X-MICROSOFT-MSNCALENDAR-ALLDAYEVENT:TRUE
+X-MS-OLK-CONFTYPE:0
+END:VEVENT
 */
