7725ce302c75d1e99af943a3d613b0c088b7d4f7
ines-rana <57879729+ines-rana@users.noreply.github.com>
2019-12-12 05:05:43 +0000

    .

--- index.js
+++ index.js
@@ -103,6 +103,7 @@ return retval;
 
 // format date string in local timezone (1997-01-06) as 19970105T220000Z
 function ical_datestr(ts){
+console.log("ical_datestr", ts)
   var td = moment.tz(ts +" 00:00:00", grTZ);
   return td.tz("UTC").format('YYYYMMDDTHHmmss') + 'Z'
 }
